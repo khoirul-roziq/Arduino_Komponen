b70b13ebd97bd88cfbaa34424f7bfe704d8a87e2
Khoirul Roziq <58505855+khoirul-roziq@users.noreply.github.com>
2020-09-04 16:05:31 +0700

    Add files via upload

---
+++ Relay/Src_Relay.ino
@@ -0,0 +1,15 @@
+#define relay 10
+
+void setup() {
+  
+  pinMode(relay, OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(relay, HIGH);
+  delay(1000);
+  digitalWrite(relay, LOW);
+  delay(1000);
+  
+}
